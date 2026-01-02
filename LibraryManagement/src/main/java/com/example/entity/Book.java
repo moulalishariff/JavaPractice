f8765601d74f96a76b293d0613feec7291a4dd0f
Moulali Shariff Mohammed <159472572+moulalishariff@users.noreply.github.com>
2026-01-02 14:03:07 +0530

    Update Book.java

--- LibraryManagement/src/main/java/com/example/entity/Book.java
+++ LibraryManagement/src/main/java/com/example/entity/Book.java
@@ -1,6 +1,10 @@
 package com.example.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @Getter
@@ -33,3 +37,4 @@ public class Book {
 	
 	
 }
+
